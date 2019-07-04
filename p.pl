a341dd4d32dc1c7d1fdb8d1a3099a6f14cc614ba
Sawyer X <xsawyerx@cpan.org>
2019-07-04 22:10:16 +0300

    Update proof-of-concept

--- p.pl
+++ p.pl
@@ -2,13 +2,13 @@ use strict;
 use warnings;
 use lib 'lib';
 
-use Python::FFI;
+use Python::FFI::Simple;
 
-my $python = Python::FFI->new();
+my $python = Python::FFI::Simple->new();
 
 my $python_code = "from time import time,ctime\n"
                 . "print('Today is', ctime(time()))\n";
 
 $python->run_simplestring($python_code);
-$python->import_importmodule('time');
+#$python->import_importmodule('time');
 
